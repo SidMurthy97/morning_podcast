a5e34ba30b2130acedbdbc6987993b5cd4c46670
Sid Murthy <murthy.siddhant@googlemail.com>
2022-03-14 21:34:03 +0000

    authorisation working. check if there is any way to remove the pasting of redirected uri

---
+++ main.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import sys
+import spotipy
+import spotipy.util as util
+import json
+from datetime import date, datetime
+from time import sleep
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
