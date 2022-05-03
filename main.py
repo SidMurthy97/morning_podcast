134303f9db89c7552e7a251bd361ab5cb7f3a9b9
Sid Murthy <murthy.siddhant@googlemail.com>
2022-05-03 21:45:03 +0100

    able to add a single podcast episode to a playlist

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ CLIENT_SECRET = os.getenv("MORNING_PODCAST_CLIENT_SECRET")
 CLIENT_URI = os.getenv("MORNING_PODCAST_REDIRECT_URI")
 SCOPE = 'playlist-modify-private playlist-modify-public'
 
+usernames = ['1184475550']
+
+podcasts_to_add = [
+    'spotify:episode:3MzCRf8RUWHpnXuKFG19qI'
+]
+
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -16,4 +22,11 @@ sp = spotipy.Spotify(
         scope=SCOPE))
 
 
-sp.user_playlist_create('1184475550', "test")
+for user in usernames: 
+    sp.user_playlist_create(user,"test")
+    created_playlist = sp.user_playlists(user,limit = 1)
+    playlist_id = created_playlist['items'][0]['id']
+    # print(playlist_id)
+    
+
+    sp.playlist_add_items(playlist_id,podcasts_to_add)
