79b0362034d86c34e319765671e37fc71443692b
Sid Murthy <murthy.siddhant@googlemail.com>
2022-05-04 18:40:25 +0100

    removed playlist creation code, added code to update an existing playlist based on the latest episode of pre-chosen episodes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
-
+import pprint
 CLIENT_ID = os.getenv("MORNING_PODCAST_CLIENT_ID")
 CLIENT_SECRET = os.getenv("MORNING_PODCAST_CLIENT_SECRET")
 CLIENT_URI = os.getenv("MORNING_PODCAST_REDIRECT_URI")
@@ -22,11 +22,27 @@ sp = spotipy.Spotify(
         scope=SCOPE))
 
 
-for user in usernames: 
-    sp.user_playlist_create(user,"test")
-    created_playlist = sp.user_playlists(user,limit = 1)
-    playlist_id = created_playlist['items'][0]['id']
-    # print(playlist_id)
+# for user in usernames: 
+#     sp.user_playlist_create(user,"test")
+#     created_playlist = sp.user_playlists(user,limit = 1)
+#     playlist_id = created_playlist['items'][0]['id']
+#     # print(playlist_id)
     
 
-    sp.playlist_add_items(playlist_id,podcasts_to_add)
+    # sp.playlist_add_items(playlist_id,podcasts_to_add)
+
+'''TODO: decide on list of episodes to add
+TODO: write function to update the playlist rather than create it each day
+TODO: Abstract episde to show level, such that the most recent episode URI can
+be got each morning.'''
+
+
+playlist_uri = 'spotify:playlist:49bH2WkTveKn9JYAjBNyfJ'
+show_uris = ['spotify:show:5CHMS8JT6vz45eRq1cGY0t'] 
+latest_episode_uri = [] #store list of all uris
+for uri in show_uris:
+    latest_episode = sp.show_episodes(uri,limit=1)
+    latest_episode_uri.append(latest_episode['items'][0]['uri'])
+
+
+sp.playlist_add_items(playlist_uri, latest_episode_uri)
