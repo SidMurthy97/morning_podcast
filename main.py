69256ae623c32a40918b237bfecd90f6c9296a69
Sid Murthy <murthy.siddhant@googlemail.com>
2022-05-05 18:32:26 +0100

    adding more shows to daily podcast list

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ SCOPE = 'playlist-modify-private playlist-modify-public'
 
 usernames = ['1184475550']
 
-podcasts_to_add = [
-    'spotify:episode:3MzCRf8RUWHpnXuKFG19qI'
-]
-
-
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -21,24 +16,13 @@ sp = spotipy.Spotify(
         redirect_uri=CLIENT_URI,
         scope=SCOPE))
 
-
-# for user in usernames: 
-#     sp.user_playlist_create(user,"test")
-#     created_playlist = sp.user_playlists(user,limit = 1)
-#     playlist_id = created_playlist['items'][0]['id']
-#     # print(playlist_id)
-    
-
-    # sp.playlist_add_items(playlist_id,podcasts_to_add)
-
-'''TODO: decide on list of episodes to add
-TODO: virtual enviroment and update requirements.txt'''
-
-
 playlist_uri = 'spotify:playlist:49bH2WkTveKn9JYAjBNyfJ'
+
 show_uris = [
     'spotify:show:2qZ0xpaBBwf3bTYhA10KZY', #daily briefing  
-    'spotify:show:29eeC2kUEpYkDIYO4Bj0Qr' #tech briefing
+    'spotify:show:29eeC2kUEpYkDIYO4Bj0Qr', #tech briefing
+    'spotify:show:1VXcH8QHkjRcTCEd88U3ti', #ted talks daily
+    'spotify:show:0hKmFWuxDIlm5IhVICVqWA'
     ] 
 
 
