339be5ce363a109f1a2a316f12b82ae5039796fc
Sid Murthy <murthy.siddhant@googlemail.com>
2022-05-24 18:42:02 +0100

    split podcasts into essential and optional. Essential episodes always get prepended. Optional episodes are subject to a duration check, and are appended in a random order

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 import pprint
+import random
+
 CLIENT_ID = os.getenv("MORNING_PODCAST_CLIENT_ID")
 CLIENT_SECRET = os.getenv("MORNING_PODCAST_CLIENT_SECRET")
 CLIENT_URI = os.getenv("MORNING_PODCAST_REDIRECT_URI")
@@ -18,18 +20,43 @@ sp = spotipy.Spotify(
 
 playlist_uri = 'spotify:playlist:49bH2WkTveKn9JYAjBNyfJ'
 
-show_uris = [
+latest_episode_uri = [] #store list of all uris
+optional_episode_uri = [] #store list of all uris
+
+#first include some tech briefings you defo want
+compulsary_show_uris = [
     'spotify:show:2qZ0xpaBBwf3bTYhA10KZY', #daily briefing  
     'spotify:show:29eeC2kUEpYkDIYO4Bj0Qr', #tech briefing
-    'spotify:show:1VXcH8QHkjRcTCEd88U3ti', #ted talks daily
-    'spotify:show:0hKmFWuxDIlm5IhVICVqWA'
     ] 
 
+#optional shows in a random order
+optional_show_uris = [
+    'spotify:show:3sVMOI29n5oyMgbUxFz0p3', #more or less 
+    'spotify:show:1410RabA4XOqO6IV8p0gYF', #FT news briefing 
+    'spotify:show:0hKmFWuxDIlm5IhVICVqWA' #sensemaker 
+]
 
-latest_episode_uri = [] #store list of all uris
-for uri in show_uris:
+#get tracks and update playlist
+
+for uri in compulsary_show_uris:
     latest_episode = sp.show_episodes(uri,limit=1)
     latest_episode_uri.append(latest_episode['items'][0]['uri'])
 
+print(latest_episode_uri)
+
+for uri in optional_show_uris:
+    latest_episode = sp.show_episodes(uri,limit=1)
+
+    #get episode duration in minutes
+    episode_duration = (latest_episode['items'][0]['duration_ms']) / (1000 * 60) 
+    
+    if episode_duration > 15:
+        break
+    
+    optional_episode_uri.append(latest_episode['items'][0]['uri'])
+
+random.shuffle(optional_episode_uri)
+latest_episode_uri += optional_episode_uri
+print(latest_episode_uri)
 
 sp.playlist_replace_items(playlist_uri, latest_episode_uri)
